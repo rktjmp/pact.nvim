0a6de5a3c9b08dbaa71f658ce2a1372ba6920a31
Oliver Marriott <hello@omarriott.com>
2023-01-22 22:55:29 +1100

    fix: container init syntax

--- container/init.lua
+++ container/init.lua
@@ -1,11 +1,11 @@
 -- Bootstrap pact if its missing, you will be instructed to install pact again
 -- the first time you run `:Pact` to finalise installation.
 if vim.loop.fs_stat(vim.fn.stdpath("data") .. "/site/pack/pact/start/pact.nvim") == nil then
+  local pactstrap_path = vim.fn.stdpath("data") .. "/site/pack/pactstrap"
   vim.notify(
-    string.format("Could not find pact.nvim, cloning new copy to %s", check_path)
+    string.format("Could not find pact.nvim, cloning new copy to %s", pactstrap_path),
     vim.log.levels.WARN
   )
-  local pactstrap_path = vim.fn.stdpath("data") .. "/site/pack/pactstrap"
   vim.fn.system({
     'git',
     'clone',
@@ -31,7 +31,7 @@ make_pact(
   github("nvim-treesitter/nvim-treesitter", {
     after = "TSInstallSync! vim lua"
   }),
-  github("nvim-lua/plenary.nvim")
+  github("nvim-lua/plenary.nvim"),
   github("some/fake-plugin.nvim", {branch = "master"}),
-  github("rktjmp/paperplanes.nvim", {version = ">= 10000.0.0"}),
+  github("rktjmp/paperplanes.nvim", {version = ">= 10000.0.0"})
 )
